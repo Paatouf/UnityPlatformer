47a785f2936d9757462940435b36908805f77280
vchagnard <v.chagnard@gmail.com>
2017-07-21 14:41:22 +0200

    [CHANGED] Animation walk added

---
+++ UnityPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+	    if(Input.GetKey(KeyCode.Q))
+        {
+           
+        }
+	}
+}
